d6a9dbcec2c8ce226084605612da03f13f4dca10
ruthel <balockruthel.cedrickascens@gmail.com>
2023-01-22 05:01:34 +0100

    update transactions

--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -1,10 +1,14 @@
 const Transaction = require("../models/transaction");
 const {ObjectId} = require("mongodb");
+const User = require("../models/user");
 
 exports.add = async (req, res) => {
   try {
     let result = new Transaction({...req.body})
-    result.save().then(doc => {
+    let user = await User.findById(req.body.owner);
+    user.balance += req.body.amount;
+    result.save().then(async doc => {
+      await user.save()
       return res.status(200).json(doc)
     }, reason => {
       return res.status(400).json(reason)
